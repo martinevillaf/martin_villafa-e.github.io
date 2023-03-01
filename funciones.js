726c74248debf5108dc0535bc5233a73654b9c6d
martinevillaf <109883432+martinevillaf@users.noreply.github.com>
2023-02-28 21:52:14 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,44 @@
+
+
+
+document.getElementById("boton").addEventListener("click",function(){
+    console.log("Cambia la visualizacion a modo oscuro")
+    document.getElementById("titu").style.background ="black"
+    document.getElementById("informacion").style.background ="black"
+    document.getElementById("informacion").style.color ="white"
+    document.getElementById("inf2").style.background ="black"
+    document.getElementById("inf2").style.color ="white"
+    document.getElementById("dom").style.color ="white"
+    document.getElementById("fecha").style.color ="white"
+    document.getElementById("correo").style.color ="white"
+    document.getElementById("web").style.color ="white"
+    document.getElementById("academica").style.color ="white"
+    document.getElementById("laboral").style.color ="white"
+    document.getElementById("redes").style.color ="white"
+    
+  
+    
+
+})
+
+document.getElementById("boton2").addEventListener("click",function(){
+    console.log("Cambia la visualizacion a modo oscuro")
+    document.getElementById("titu").style.background ="white"
+    document.getElementById("informacion").style.background ="white"
+    document.getElementById("informacion").style.color ="black"
+    document.getElementById("inf2").style.background ="white"
+    document.getElementById("inf2").style.color ="black"
+    document.getElementById("dom").style.color ="black"
+    document.getElementById("fecha").style.color ="black"
+    document.getElementById("correo").style.color ="black"
+    document.getElementById("web").style.color ="black"
+    document.getElementById("academica").style.color ="black"
+    document.getElementById("laboral").style.color ="black"
+    document.getElementById("redes").style.color ="black"
+    
+  
+})
+
+    
+
+
